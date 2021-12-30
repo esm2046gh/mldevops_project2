cb73e075fa594529a234a074f4f1007cbf1d27b6
esm <esm@mymail.com>
2021-12-30 02:33:46 +0100

    Before saving the cleaned artifact. Keep records within the coordinates     idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)     df = df[idx].copy()
        df.to_csv(args.output_artifact, index=False)

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -37,6 +37,8 @@ def go(args):
 
     # 3)  Saving the cleaned artifact
     logger.info(f"Saving cleaned artifact as '{args.output_artifact}'")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
     df.to_csv(args.output_artifact, index=False)
 
     artifact = wandb.Artifact(
